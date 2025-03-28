6c178f5be9073d117f92d18dac06f9e5a39c554b
Miah <harpermiah@yahoo.com>
2025-03-28 11:45:05 -0400

    updated target framework from .net6 to .net9

--- IterationStatements/obj/Debug/net6.0/IterationStatements.AssemblyInfo.cs
+++ IterationStatements/obj/Debug/net6.0/IterationStatements.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557cf744728aa137aa42937775e92ea64f0f173c")]
 [assembly: System.Reflection.AssemblyProductAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IterationStatements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
